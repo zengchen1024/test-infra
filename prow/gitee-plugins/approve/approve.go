379608abea162af7f338fdfb75452494f39d6bd7
zengchen1024 <chenzeng2@huawei.com>
2020-04-18 16:17:25 +0800

    implement gitee approve

--- prow/gitee-plugins/approve/approve.go
+++ prow/gitee-plugins/approve/approve.go
@@ -2,6 +2,7 @@ package approve
 
 import (
 	"fmt"
+	"net/url"
 
 	"gitee.com/openeuler/go-gitee/gitee"
 	"github.com/sirupsen/logrus"
@@ -48,24 +49,19 @@ func NewApprove(f plugins.GetPluginConfig, ghc githubClient, oc ownersClient) pl
 }
 
 func (a *approve) HelpProvider(enabledRepos []prowConfig.OrgRepo) (*pluginhelp.PluginHelp, error) {
-	c := a.getPluginConfig(a.PluginName())
-	if c == nil {
-		return nil, fmt.Errorf("can't find the approve's configuration")
+	c, err := a.pluginConfig()
+	if err != nil {
+		return nil, err
 	}
 
-	c1, ok := c.(*configuration)
-	if !ok {
-		return nil, fmt.Errorf("can't convert to approve's configuration")
-	}
-
-	c2 := originp.Configuration{Approve: c1.Approve}
+	c1 := originp.Configuration{Approve: c.Approve}
 
 	h, ok := originp.HelpProviders()[a.PluginName()]
 	if !ok {
 		return nil, fmt.Errorf("can't find the approve's original helper method")
 	}
 
-	return h(&c2, enabledRepos)
+	return h(&c1, enabledRepos)
 }
 
 func (a *approve) PluginName() string {
@@ -87,13 +83,68 @@ func (a *approve) NewPluginConfig() plugins.PluginConfig {
 func (a *approve) RegisterEventHandler(p plugins.Plugins) {
 	name := a.PluginName()
 	p.RegisterNoteEventHandler(name, a.handleNoteEvent)
-	p.RegisterPullRequestHandler(name, a.handlePullRequestEvent)
+	//p.RegisterPullRequestHandler(name, a.handlePullRequestEvent)
 }
 
 func (a *approve) handleNoteEvent(e *gitee.NoteEvent, log *logrus.Entry) error {
-	return nil
+	pr := e.PullRequest
+	org := e.Repository.Owner.Login
+	repo := e.Repository.Name
+
+	repoc, err := a.oc.LoadRepoOwners(org, repo, pr.Base.Ref)
+	if err != nil {
+		return err
+	}
+
+	c, err := a.approveFor(org, repo)
+	if err != nil {
+		return err
+	}
+
+	assignees := make([]github.User, 0, len(pr.Assignees))
+	for i, item := range pr.Assignees {
+		assignees[i] = github.User{Login: item.Login}
+	}
+
+	return origina.Handle(
+		log,
+		a.ghc,
+		repoc,
+		getGiteeOption(),
+		c,
+		origina.NewState(org, repo, pr.Base.Ref, pr.Body, pr.User.Login, pr.HtmlUrl, int(pr.Number), assignees),
+	)
 }
 
 func (a *approve) handlePullRequestEvent(e *gitee.PullRequestEvent, log *logrus.Entry) error {
 	return nil
+}
+
+func getGiteeOption() prowConfig.GitHubOptions {
+	s := "https://gitee.com"
+	linkURL, _ := url.Parse(s)
+	return prowConfig.GitHubOptions{LinkURLFromConfig: s, LinkURL: linkURL}
+}
+
+func (a *approve) pluginConfig() (*configuration, error) {
+	c := a.getPluginConfig(a.PluginName())
+	if c == nil {
+		return nil, fmt.Errorf("can't find the approve's configuration")
+	}
+
+	c1, ok := c.(*configuration)
+	if !ok {
+		return nil, fmt.Errorf("can't convert to approve's configuration")
+	}
+	return c1, nil
+}
+
+func (a *approve) approveFor(org, repo string) (*originp.Approve, error) {
+	c, err := a.pluginConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	c1 := originp.Configuration{Approve: c.Approve}
+	return c1.ApproveFor(org, repo), nil
 }
