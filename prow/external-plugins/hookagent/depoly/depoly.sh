2749e80dc1ed55e3d827e593299674836576099c
xwzQmxx <1499273991@qq.com>
2021-02-03 11:21:23 +0800

    modify dockerfile

---
+++ prow/external-plugins/hookagent/depoly/depoly.sh
@@ -0,0 +1,4 @@
+echo -e "[dailybuild_factory]\nname=dailybuild_factory\nbaseurl=http://119.3.219.20:82/openEuler:/Factory/standard_x86_64/\nenabled=1\ngpgcheck=0\n\n[dailybuild_mainline]\nname=dailybuild_mainline\nbaseurl=http://119.3.219.20:82/openEuler:/Mainline/standard_x86_64/\nenabled=1\ngpgcheck=0" > /etc/yum.repos.d/openEuler.repo
+yum clean all
+yum makecache
+yum -y install openEuler-Advisor
